85b574a274a8ca3c771d6d49a29f99eee028cbee
sarahawilson <sarahwilson0800@live.com>
2022-01-30 19:02:04 -0600

    Have some data being read in to a DataTable. Can print. Need to actually do on a real data set next.

---
+++ Module1/MLPL/Helper.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+
+namespace MLPL.Helper
+{
+    public class DataManager
+    {
+        DataTable thisData = new DataTable();
+
+
+        public void LoadDataSet() 
+        {
+            Console.WriteLine("Loading in ML Data Set");
+            //TODO: Implement 
+            thisData.Columns.Add("Name");
+            thisData.Columns.Add("Email");
+
+            DataRow curRow = thisData.NewRow();
+            curRow["Name"] = "Sarah";
+            curRow["Email"] = "Sarah@gmail.com";
+
+            thisData.Rows.Add(curRow);
+        }
+
+        public void PrintDataSet()
+        {
+            //TODO: Implement
+            foreach (DataRow row in thisData.Rows) 
+            {
+                string Name = row["Name"].ToString();
+                Console.WriteLine(Name);
+                string Email = row["Email"].ToString();
+                Console.WriteLine(Email);
+            }
+        }
+
+        public void PrintSomething()
+        {
+            Console.WriteLine("Something");
+        }
+    }
+}
---
+++ Module1/MLPL/Program.cs
@@ -0,0 +1,16 @@
+﻿using MLPL.Helper;
+
+namespace MLPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Machine Learning Pipeline (MLPL)");
+            DataManager myHelper = new DataManager();
+            myHelper.PrintSomething();
+            myHelper.LoadDataSet();
+            myHelper.PrintDataSet();
+        }
+    }
+}
