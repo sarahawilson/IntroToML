b9534c37e39731cfb1f72597bbcd46fc5e776fa4
sarahawilson <sarahwilson0800@live.com>
2022-02-01 20:15:51 -0600

    Stuck in pandas hell.... please send help..... can't last much longer.......

--- Module1/MLPL/MLPL/DataHelper.py
+++ Module1/MLPL/MLPL/DataHelper.py
@@ -1,10 +1,32 @@
 import pandas as pd 
+import numpy as np
 
 class DataHelper(object):
-    def __init__(self, dataSetFilePath):
+    def __init__(self, dataSetFilePath, hasHeader=True):
         self.ID = 'Helper_01'
         self.filePath = dataSetFilePath
-        self.dataFrame = pd.read_csv(self.filePath)   
-
+        if(hasHeader):
+            self.dataFrame = pd.read_csv(self.filePath)
+        else:
+            self.dataFrame = pd.read_csv(self.filePath, header=None)
+            
     def printData(self):
         print(self.dataFrame)
+    
+    def handleMissingValues(self, missingRepresentation=None):
+        #Waiting on Response from discussion question
+        #Write a function that, given a dataset, imputes missing
+        #values with the feature (column) mean
+        #For now assuming that this implies the whole column
+        
+        #Get the average of the whole column 
+        if(missingRepresentation != None): 
+            self.dataFrame.replace({missingRepresentation: None}, inplace=True)
+            print(self.dataFrame)
+            testMeanCol = self.dataFrame.mean(axis=0, skipna=True)
+            test2 = self.dataFrame[1].mean()
+            print(testMeanCol)
+            print(test2)
+            print(self.dataFrame.isna())
+        
+        
--- Module1/MLPL/MLPL/MLPL.py
+++ Module1/MLPL/MLPL/MLPL.py
@@ -1,7 +1,46 @@
+#Sarah Wilson 
+
+
 import DataHelper
 
 if __name__ == "__main__":
     print("MLPL - Machine Learning Pipeline")
+    
+    #Data File Paths
     abaloneDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\Abalone\abalone.data"
-    myDH_abalone = DataHelper.DataHelper(abaloneDataSet)
-    myDH_abalone.printData()
+    carEvalDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\CarEvaluation\car.data"
+    breastCancerDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\BreastCancer\breast-cancer-wisconsin.data"
+    compHardwareDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\ComputerHardware\machine.data"
+    congVoteDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\CongressionalVote\house-votes-84.data"
+    forestFireDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\ForestFires\forestfires.data"
+    
+    testAvgDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\simpleTestDataSets\colAvg.data"
+    
+    dsLocations = [abaloneDataSet, carEvalDataSet, breastCancerDataSet, compHardwareDataSet, congVoteDataSet, forestFireDataSet]
+    
+    #Albalone Data Set NH 
+    #Car Data Set has NMV NH
+    #Forest Fires has NMV Header
+    #
+    
+    myDH_Abalone = DataHelper.DataHelper(abaloneDataSet, False)
+    myDH_CarEval = DataHelper.DataHelper(carEvalDataSet, False)
+    myDH_BreastCancer = DataHelper.DataHelper(breastCancerDataSet, False)
+    myDH_CompHW = DataHelper.DataHelper(compHardwareDataSet, True)
+    myDH_CongVote = DataHelper.DataHelper(congVoteDataSet, False)
+    myDH_ForestFire = DataHelper.DataHelper(forestFireDataSet, True)
+    
+    myDH_TestAvg = DataHelper.DataHelper(testAvgDataSet, False)
+    
+    #Handle the Missing Data
+    myDH_Abalone.handleMissingValues() #No Missing Data
+    myDH_CarEval.handleMissingValues() #No Missing Data
+    #myDH_BreastCancer.handleMissingValues('?') #Missing Value represented by ?
+    myDH_CompHW.handleMissingValues() #No Missing Data
+    myDH_CongVote.handleMissingValues() #No Missing Data (? has different meaning)
+    myDH_ForestFire.handleMissingValues() #No Missing Data 
+    
+    
+    myDH_TestAvg.printData()
+    myDH_TestAvg.handleMissingValues("?")
+    myDH_TestAvg.printData()
