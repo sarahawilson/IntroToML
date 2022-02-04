4d291adc80e6e5ac313e2697c33deb130cb8b899
sarahawilson <sarahwilson0800@live.com>
2022-02-04 16:36:45 -0600

    Disct is working.

--- Module1/MLPL/MLPL/MLPL.py
+++ Module1/MLPL/MLPL/MLPL.py
@@ -1,6 +1,5 @@
 #Sarah Wilson 
 
-import DataHelper
 import pandas as pd
 from typing import List, Tuple, Dict;
 
@@ -13,78 +12,93 @@ def main(dataFile: str,
          nominalOneHotColList: List = None,
          discretizationApply: List = None):
     
-    dataSteps = []  #List that will store the steps as the data gets prepped for processing
+    # Dictonary that will store the data frame states as it gets certain types
+    # of transformations applied. Will allow for debugging and demonstraitons
     dataProc = {'Raw Data': None,
                  'Raw Data dTypes Applied': None,
-                 'Average On Features': None,
+                 'Int. Step Average On Features': None,
                  'Filled Data': None,
                  'Ordinal Encoded Data': None,
                  'Nominal One Hot Data': None,
-                 'Discretization Equal Width': None,
-                 'Discretization Equal Freq': None}
+                 'Int. Step Discretization Equal Width': None,
+                 'Int. Step Discretization Equal Freq': None,
+                 'DataFrame Discretization Equal Width': None,
+                 'DataFrame Discretization Equal Freq': None}
     
     #TODO: Update this to a dictonary
     
     raw_data = pd.read_csv(dataFile, names=dataFileHeaders)
-    dataSteps.append(raw_data)
-    dataProc['Raw Data'] = pd.read_csv(dataFile, names=dataFileHeaders)
+    dataProc['Raw Data'] = raw_data
     
     raw_data_typeApplied = pd.read_csv(dataFile, names=dataFileHeaders, dtype=dataFileDtypes, converters=dataFileDtypConvtert)
-    dataSteps.append(raw_data_typeApplied)
-    dataProc['Raw Data dTypes Applied'] = pd.read_csv(dataFile, names=dataFileHeaders)
+    dataProc['Raw Data dTypes Applied'] = raw_data_typeApplied
     
-    filled_data = raw_data_typeApplied.copy(deep=True)
-    
+
     #Apply Transform needed for Missing Attribute Values
+    filled_data = raw_data_typeApplied.copy(deep=True)
     if (missingValuesCols != None):
         mean = []
         for col in missingValuesCols:
             mean.append(raw_data_typeApplied[col].mean(axis=0, skipna=True))
-        dataSteps.append(mean)
+        dataProc['Int. Step Average On Features'] = mean
         colIndex = 0
         for col in missingValuesCols:
             filled_data.fillna({col: mean[colIndex]}, inplace=True)
             colIndex = colIndex + 1
-        dataSteps.append(filled_data)
-    else:
-        dataSteps.append('No Missing Values: Mean NA')
-        dataSteps.append(filled_data)
-    
+        dataProc['Filled Data'] = filled_data
 
+    
+    #Apply Transform needed for converting Ordinal Data to Ints
     ordinalEncoded_data = filled_data.copy(deep=True)
-    #Apply Transform needed for converting Ordinal Data to Ints   
     if (ordinalEncoding != None):
         ordinalEncoded_data.replace(to_replace=ordinalEncoding, inplace = True)
-        #for col_header, encoding in ordinalEncoding.items():
-            #ordinalEncoded_data
-        dataSteps.append(ordinalEncoded_data)
-    else:
-        #This is just to keep the overall list structure in tact (Might want to make this a dict)
-        dataSteps.append(ordinalEncoded_data)
+        dataProc['Ordinal Encoded Data'] = ordinalEncoded_data
             
-    #nominalOneHotEncoded_data = ordinalEncoded_data.copy(deep=True)  
+    #Apply Transform needed for converting Nominal Data into a One Hot Encoding
+    nominalOneHotBase_data = filled_data.copy(deep=True)
     if(nominalOneHotColList != None):
-        nominalOneHotEncoded_data = pd.get_dummies(filled_data, columns = nominalOneHotColList)
-        dataSteps.append(nominalOneHotEncoded_data)
-    else:
-        dataSteps.append(ordinalEncoded_data)
+        nominalOneHotEncoded_data = pd.get_dummies(nominalOneHotBase_data, columns = nominalOneHotColList)
+        dataProc['Nominal One Hot Data'] = nominalOneHotEncoded_data
+
         
     #Apply the Discritiztion 
-    discretization_data = filled_data.copy(deep=True)
+    discretization_data_equalWidth = filled_data.copy(deep=True)
+    discretization_data_equalFrequency = filled_data.copy(deep=True)
     if(discretizationApply != None):
         discType = discretizationApply[0]
         numBins = discretizationApply[1]
+        discEqualWidthCats = []  #Intermediate Form 
+        discEqualFreqCats= []    #Intermediate Form
         for colHeader in discretizationApply[2]:
-            if discType == 'Even Width':
-                discCol = pd.qcut(x= discretization_data[colHeader], q = numBins)
-                discColCut = pd.cut(discretization_data[colHeader], numBins)
-    else:
-        discCol = None
-           
-    test = discCol   
-        
-        
-    #return dataSteps
+            if discType == 'Equal Width':
+                #Intermediate Form for debug and verification
+                discEqualWidthCats.append(pd.cut(discretization_data_equalWidth[colHeader], numBins))
+                
+                #Apply to the real data frame
+                discretization_data_equalWidth[colHeader] = (pd.cut(discretization_data_equalWidth[colHeader], numBins))
+                
+                #Get the Count to Display to the Console for Demonstartion
+                print('Equal Width Demo:')
+                print(discretization_data_equalWidth[colHeader].value_counts())
+                
+            if discType == 'Equal Frequency':
+                #Intermediate Form for debug and verification
+                discEqualFreqCats.append(pd.qcut(discretization_data_equalFrequency[colHeader], numBins, duplicates = 'drop'))
+                
+                #Apply to the real data frame
+                discretization_data_equalFrequency[colHeader] = (pd.qcut(discretization_data_equalFrequency[colHeader], numBins, duplicates = 'drop'))
+                
+                #Get the Count to Display to the Console for Demonstartion
+                
+                print('Equal Frequency Demo:')
+                print(discretization_data_equalFrequency[colHeader].value_counts())
+                
+                
+        if discType == 'Equal Width':
+            dataProc['Int. Step Discretization Equal Width'] = discEqualWidthCats
+        if discType == 'Equal Frequnecy':
+            dataProc['Int. Step Discretization Equal Freq'] = discEqualFreqCats
+                
     return dataProc
     
     #categoial data on the mean -> if it's a stirng / yes or no just use the most coming occuring value
@@ -192,7 +206,14 @@ if __name__ == "__main__":
     testDiscEWDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\simpleTestDataSets\discEvenWidth.data"
     testDiscEWHeaders = ['EWCol','Val1','Val2']
     #DiscList format is Type of Disc / Number of Bins /  List Column to apply on
-    testDiscEWDiscList = ['Even Width', 10, ['EWCol']]
+    testDiscEWDiscList = ['Equal Width', 10, ['EWCol']]
+    
+    #This Test set shows Even Frequency Discretizaiton
+    testDiscEFDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\simpleTestDataSets\discEvenFreq.data"
+    testDiscEFHeaders = ['EFCol','Val1','Val2']
+    #DiscList format is Type of Disc / Number of Bins /  List Column to apply on
+    testDiscEFDiscList = ['Equal Frequency', 3, ['EFCol']]
+    
     
     dfSteps_Abalone = main(abaloneDataSet, abaloneHeaders, abaloneDtypeDict)
     dfSteps_BreastCancer = main(breastCancerDataSet, breastCancerHeaders, breastCancerDtypes, breastCancerDtypeConvterts, breastCancerMissingValCols)
@@ -206,6 +227,7 @@ if __name__ == "__main__":
     dfSteps_testOrdinal = main(testOrdinalDataSet, testOrdinalHeaders, testOrdinalDtypeDict, None, None, testOrdinalEncodingDict)
     dfSteps_testNomOneHot = main(testNomOneHotDataSet, testNomOneHotlHeaders, testNomOneHotDtypeDict, None, None, None, testNomOneHotColList)
     dfSteps_testDicEvenWidth = main(testDiscEWDataSet, testDiscEWHeaders, None, None, None, None, None, testDiscEWDiscList)
+    dfSteps_testDicEvenFreq = main(testDiscEFDataSet, testDiscEFHeaders, None, None, None, None, None, testDiscEFDiscList)
     
     
     
