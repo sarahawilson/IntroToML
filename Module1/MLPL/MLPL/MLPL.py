c0b816bfd1598221eeca602b7990f9e1c1932290
sarahawilson <sarahwilson0800@live.com>
2022-02-04 19:03:27 -0600

    Added the overall data set supervised learned type.

--- Module1/MLPL/MLPL/MLPL.py
+++ Module1/MLPL/MLPL/MLPL.py
@@ -112,18 +112,11 @@ if __name__ == "__main__":
     print("MLPL - Machine Learning Pipeline")
     
     
-    ####################
-    # ALBALONDE DATA
-    ####################
-    abaloneDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\Abalone\abalone.data"
-    abaloneHeaders = ['Sex', 'Length', 'Diameter', 'Height', 'Whole Weight', 'Shucked Weight', 'Viscera Weight', 'Shell Weight', 'Rings']
-    abaloneDtypeDict = {'Sex': 'str', 'Length': 'float' , 'Diameter': 'float', 'Height': 'float', 
-                        'Whole Weight':'float', 'Shucked Weight':'float', 'Viscera Weight': 'float', 'Shell Weight': 'float', 'Rings': 'int'}
-
 
     ####################
     # BREAST CANCER DATA
     ####################
+    breastCancerDataSet_OverallType = 'Classification'
     breastCancerDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\BreastCancer\breast-cancer-wisconsin.data"
     breastCancerHeaders = ['Sample Code Number', 'Clump Thickness', 'Uni. of Cell Size', 'Uni. of Cell Shape', 'Marginal Adhesion', 'Single Ep. Cell Size', 'Bare Nuclei',
                             'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
@@ -141,40 +134,48 @@ if __name__ == "__main__":
     ####################
     # CAR DATA
     ####################
+    carEvalDataSet_OverallType = 'Classification'
     carEvalDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\CarEvaluation\car.data"
     carEvalHeaders = ['Buying', 'Maint', 'Doors', 'Persons', 'Lug_Boot', 'Safety', 'Car Acceptability']
     carDtypeDict = {'Buying': 'str', 'Maint': 'str', 'Lug_Boot': 'str', 'Safety': 'str', 'Car Acceptability': 'str'}
     carDtypeConvterts = {'Doors': convert_StringToIntOrNaN, 'Persons': convert_StringToIntOrNaN}
     #TODO: handle the 5-more (just make value 5 for doors)
     #TODO: handle the more (just make the value max+1 for persons)
+ 
+    
+    ####################
+    # CONGRESSIONAL VODE DATA
+    ####################
+    congVoteDataSet_OverallType = 'Classification'
+    congVoteDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\CongressionalVote\house-votes-84.data"
+    congVoteHeaders = ['Class Name', 'handicapped-infants', 'water-project-cost-sharing', 'adoption-of-the-budget-resolution', 'physician-fee-freeze', 'el-salvador-aid',
+                       'religious-groups-in-schools', 'anti-satellite-test-ban', 'aid-to-nicaraguan-contras', 'mx-missile', 'immigration', 'synfuels-corporation-cutback',
+                       'education-spending', 'superfund-right-to-sue', 'crime', 'duty-free-exports', 'export-administration-act-south-africa']
+    
     
     
     ####################
     # COMPUTER HARDWARE DATA
     ####################
+    compHardwareDataSet_OverallType = 'Regression'
     compHardwareDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\ComputerHardware\machine.data"
     compHardwareHeader = ['Vendor Name','Model Name','MYCT','MMIN','MMAX','CACH','CHMIN','CHMAX','PRP','ERP']
     compHardwareDtypeDict = {'Vendor Name': 'str','Model Name': 'str','MYCT': 'int','MMIN': 'int',
                              'MMAX': 'int','CACH': 'int','CHMIN': 'int','CHMAX': 'int','PRP': 'int','ERP': 'int'}
     
-    
+     ####################
+    # ALBALONDE DATA
     ####################
-    # CONGRESSIONAL VODE DATA
-    ####################
-    congVoteDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\CongressionalVote\house-votes-84.data"
-    congVoteHeaders = ['Class Name', 'handicapped-infants', 'water-project-cost-sharing', 'adoption-of-the-budget-resolution', 'physician-fee-freeze', 'el-salvador-aid',
-                       'religious-groups-in-schools', 'anti-satellite-test-ban', 'aid-to-nicaraguan-contras', 'mx-missile', 'immigration', 'synfuels-corporation-cutback',
-                       'education-spending', 'superfund-right-to-sue', 'crime', 'duty-free-exports', 'export-administration-act-south-africa']
-    #TODO: This data seems like it would be a good candiate for the 1 hot encoding
-    #basically there is Voted Yes / Voted No / Netural 
-    #YES = 100
-    #No = 010
-    #Neautral = 000
-    
-    
+    abaloneDataSet_OverallType = 'Regression'
+    abaloneDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\Abalone\abalone.data"
+    abaloneHeaders = ['Sex', 'Length', 'Diameter', 'Height', 'Whole Weight', 'Shucked Weight', 'Viscera Weight', 'Shell Weight', 'Rings']
+    abaloneDtypeDict = {'Sex': 'str', 'Length': 'float' , 'Diameter': 'float', 'Height': 'float', 
+                        'Whole Weight':'float', 'Shucked Weight':'float', 'Viscera Weight': 'float', 'Shell Weight': 'float', 'Rings': 'int'}
+
     ####################
     # FOREST FIRE DATA
     ####################
+    forestFireDataSet_OverallType = 'Regression'
     forestFireDataSet = r"C:\Users\Sarah Wilson\Desktop\JHU Classes\IntroToML\DataSets\ForestFires\forestfires.data"
     
     
